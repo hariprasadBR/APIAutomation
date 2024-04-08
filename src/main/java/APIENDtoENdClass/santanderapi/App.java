73f76263ff0798f59a0db902b36a8a78af3a27bc
hariprasad <hariprasadnomula92@gmail.com>
2024-04-08 19:36:45 +0530

    added from 92

---
+++ src/main/java/APIENDtoENdClass/santanderapi/App.java
@@ -0,0 +1,13 @@
+package APIENDtoENdClass.santanderapi;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
